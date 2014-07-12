50877fb53b186e5cef12da15ce6d042aa23b23de
Flanterroy <gustavo.sistemas@hotmail.com>
2014-07-12 00:42:19 -0300

    classe de login

---
+++ doarSP/src/com/app/doarsp/Login.java
@@ -0,0 +1,116 @@
+package com.app.doarsp;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import java.util.HashMap;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.app.*;
+
+@SuppressLint("ValidFragment")
+public class Login extends Fragment{
+	
+	Utils util;
+	
+	private String login, senha;
+	
+	/** Variaveis da Sessão **/
+	
+	// Preferencias
+    SharedPreferences pref;
+    
+    // Editor das preferencias
+    Editor editor;
+   
+    // Context
+    Context _context;
+    
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+    
+    // Sharedpref file name
+    private static final String PREF_NAME = "AndroidHivePref";
+    
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+     
+    // Email do usuário
+    public static final String KEY_EMAIL = "email";
+    
+    /** Fim Variaveis da Sessão **/
+	
+	@Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                             Bundle savedInstanceState) {
+	        // Inflate the layout for this fragment
+	        return inflater.inflate(R.layout.fragment_login, container, false);
+	    }
+	
+		public Login(Context context){
+			this._context = context;
+	        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+	        editor = pref.edit();
+		}
+		
+		//método que valida o login do usuário
+		public boolean validaLogin(){
+			return true;
+		}
+		
+		
+		//Método que cria a sessão
+		public void criarSessao(String email){
+			
+			editor.putBoolean(IS_LOGIN, true);
+			editor.putString(KEY_EMAIL, email);
+			editor.commit();
+			
+		}
+		
+		//Método que checa sessão
+		public boolean checarSessao(){
+		        return pref.getBoolean(IS_LOGIN, false);
+		}
+		
+		//Método que destroi a sessão
+		public void destroiSessao(){
+			
+			editor.clear();
+			editor.commit();
+			
+		}
+		
+		/** Métodos set e get da classe **/
+		
+		public void setLogin(String login){
+			this.login = login;
+		}
+		
+		public void setSenha(String senha){
+			this.senha = util.toSHA1(senha.getBytes());
+		}
+		
+		public String getLogin(){
+			return login;
+		}
+		
+		public String getSenha(){
+			return senha;
+		}
+		
+		/** Fim Métodos set e get da classe **/
+}
