2d519f35b08a0d76f1a47a204bb9631a66ab5679
Flanterroy <gustavo.sistemas@hotmail.com>
2014-10-27 09:42:09 -0200

    GCM

---
+++ doarSP/src/com/app/gcm/Notificacao.java
@@ -0,0 +1,22 @@
+package com.app.gcm;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.os.*;
+import android.os.Bundle;
+
+
+public class Notificacao{
+
+
+	public void gerarNotificacao(View view){
+		
+	
+	}
+	
+	
+}	
+	
+
